e26d1ce45f8a5574a81fa026cf09e4cd3095f159
Philipp Reh <sefi@s-e-f-i.de>
2013-02-15 15:49:08 +0100

    Added missing ROFL_SYMBOL

--- include/rofl/graph/simplify.hpp
+++ include/rofl/graph/simplify.hpp
@@ -1,6 +1,7 @@
 #ifndef ROFL_GRAPH_SIMPLIFY_HPP_INCLUDED
 #define ROFL_GRAPH_SIMPLIFY_HPP_INCLUDED
 
+#include <rofl/symbol.hpp>
 #include <rofl/graph/object_fwd.hpp>
 
 
@@ -9,6 +10,7 @@ namespace rofl
 namespace graph
 {
 
+ROFL_SYMBOL
 void
 simplify(
 	rofl::graph::object &
