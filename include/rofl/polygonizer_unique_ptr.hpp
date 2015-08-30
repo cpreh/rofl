9ac6bd08d0db25464f4fa0900fd166e6ed48465b
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-08-30 11:09:03 +0200

    Use fcppt::unique_ptr

--- include/rofl/polygonizer_unique_ptr.hpp
+++ include/rofl/polygonizer_unique_ptr.hpp
@@ -2,15 +2,13 @@
 #define ROFL_POLYGONIZER_UNIQUE_PTR_HPP_INCLUDED
 
 #include <rofl/polygonizer_fwd.hpp>
-#include <fcppt/config/external_begin.hpp>
-#include <memory>
-#include <fcppt/config/external_end.hpp>
+#include <fcppt/unique_ptr_impl.hpp>
 
 
 namespace rofl
 {
 
-typedef std::shared_ptr<
+typedef fcppt::unique_ptr<
 	rofl::polygonizer
 > polygonizer_unique_ptr;
 
--- src/create_polygonizer.cpp
+++ src/create_polygonizer.cpp
@@ -1,14 +1,20 @@
 #include <rofl/create_polygonizer.hpp>
+#include <rofl/polygonizer.hpp>
 #include <rofl/polygonizer_unique_ptr.hpp>
 #include <rofl/aux/polygonizers/triangle/object.hpp>
-#include <fcppt/make_unique_ptr.hpp>
+#include <fcppt/make_unique_ptr_fcppt.hpp>
+#include <fcppt/unique_ptr_to_base.hpp>
 
 
 rofl::polygonizer_unique_ptr
 rofl::create_polygonizer()
 {
 	return
-		fcppt::make_unique_ptr<
-			rofl::aux::polygonizers::triangle::object
-		>();
+		fcppt::unique_ptr_to_base<
+			rofl::polygonizer
+		>(
+			fcppt::make_unique_ptr_fcppt<
+				rofl::aux::polygonizers::triangle::object
+			>()
+		);
 }
