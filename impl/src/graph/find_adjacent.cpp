3839e9ae609749614c7696af855b1e44ef9314e2
Carl Philipp Reh <carlphilippreh@gmail.com>
2024-03-30 18:19:08 +0100

    Replace assert/unreachable

--- impl/src/graph/find_adjacent.cpp
+++ impl/src/graph/find_adjacent.cpp
@@ -2,76 +2,38 @@
 #include <rofl/indexed_polygon.hpp>
 #include <rofl/aux/graph/find_adjacent.hpp>
 #include <rofl/aux/graph/placement.hpp>
-#include <fcppt/assert/unreachable.hpp>
+#include <fcppt/exception.hpp>
+#include <fcppt/text.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <iterator>
 #include <utility>
 #include <fcppt/config/external_end.hpp>
 
-
-std::pair<
-	rofl::indexed_point,
-	rofl::aux::graph::placement
->
-rofl::aux::graph::find_adjacent(
-	rofl::indexed_polygon const &_p,
-	rofl::indexed_point const &_a,
-	rofl::indexed_point const &_b
-)
+std::pair<rofl::indexed_point, rofl::aux::graph::placement> rofl::aux::graph::find_adjacent(
+    rofl::indexed_polygon const &_p, rofl::indexed_point const &_a, rofl::indexed_point const &_b)
 {
-	for(
-		auto it(
-			_p.begin()
-		);
-		it != _p.end();
-		++it
-	)
-	{
-		if(
-			(*it) != _a
-		)
-		{
-			continue;
-		}
+  for (auto it(_p.begin()); it != _p.end(); ++it)
+  {
+    if ((*it) != _a)
+    {
+      continue;
+    }
 
-		rofl::indexed_point const &next{
-			it
-			==
-			std::prev( // NOLINT(fuchsia-default-arguments-calls)
-				_p.end()
-			)
-			?
-				_p.front()
-			:
-				*std::next( // NOLINT(fuchsia-default-arguments-calls)
-					it
-				)
-		};
+    rofl::indexed_point const &next{
+        it == std::prev( // NOLINT(fuchsia-default-arguments-calls)
+                  _p.end())
+            ? _p.front()
+            : *std::next( // NOLINT(fuchsia-default-arguments-calls)
+                  it)};
 
-		rofl::indexed_point const &prev{
-			it == _p.begin()
-			?
-				_p.back()
-			:
-				*std::prev( // NOLINT(fuchsia-default-arguments-calls)
-					it
-				)
-		};
+    rofl::indexed_point const &prev{
+        it == _p.begin() ? _p.back()
+                         : *std::prev( // NOLINT(fuchsia-default-arguments-calls)
+                               it)};
 
-		return
-			next == _b
-			?
-				std::make_pair(
-					prev,
-					rofl::aux::graph::placement::previous
-				)
-			:
-				std::make_pair(
-					next,
-					rofl::aux::graph::placement::next
-				)
-			;
-	}
+    return next == _b ? std::make_pair(prev, rofl::aux::graph::placement::previous)
+                      : std::make_pair(next, rofl::aux::graph::placement::next);
+  }
 
-	FCPPT_ASSERT_UNREACHABLE;
+  throw fcppt::exception{FCPPT_TEXT("find_adjacent failed!")};
 }
